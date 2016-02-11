633477dee60f5277b01f4b60132585ddc96d86ad
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2016-02-11 17:38:49 +1100

    added guard against defining namespace using reserved word

--- lib/traction.rb
+++ lib/traction.rb
@@ -3,6 +3,7 @@ require "traction/web_connection"
 module Traction
   def self.configure(password, function_paths={})
     function_paths.each do |name, path|
+      raise ArgumentError, "!ERROR: Unable to define a function path with name: #{name} as this is a reserved name." if self.class.respond_to?(name)
       self.class.send(:define_method, name, -> { WebConnection.new(path, password) })
     end
   end
